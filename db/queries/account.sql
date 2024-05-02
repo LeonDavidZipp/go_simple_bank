2afc1785ed5262f5d44d0d9bfce50258d5ce6dce
LeondavidZipp <leondavidzipp@gmx.de>
2024-05-02 16:55:11 +0000

    added more queries

--- db/queries/account.sql
+++ db/queries/account.sql
@@ -8,3 +8,10 @@ INSERT INTO accounts (
     $2,
     $3
 ) RETURNING *;
+
+
+-- name: GetAccount :one
+SELECT * FROM accounts WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts ORDER BY id LIMIT $1 OFFSET $2;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LeonDavidZipp/go_simple_bank
+
+go 1.22.2
