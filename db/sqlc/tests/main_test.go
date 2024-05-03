b7d9431b5d97af7b1dfc6fab483811aaadece63b
LeondavidZipp <leondavidzipp@gmx.de>
2024-05-03 12:17:35 +0000

    added env vars to Dockerfile

--- db/sqlc/tests/main_test.go
+++ db/sqlc/tests/main_test.go
@@ -2,9 +2,16 @@ package db
 
 import (
 	"testing"
+	"database/sql"
+	"os"
 )
 
 var testQueries *Queries;
 
 
 funct TestMain(m *testing.M) {
+	conn, err := sql.Open(
+		os.Getenv("dbDriver"),
+		os.Gertenv("dbSource")
+	);
+}
