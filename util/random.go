65c09bc2227dd2b6f473579e35b2cc89fb6d2d33
Leon David Zipp <leondavidzipp@gmx.de>
2024-05-04 21:38:58 +0200

    fixed utils

--- util/random.go
+++ util/random.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -13,12 +14,12 @@ func RandomInt(min int64, max int64) int64 {
 	return min + rand.Int63n(max - min + 1)
 }
 
-func RandomStr(len int) string {
+func RandomStr(strlen int) string {
 	const alphabet = "abcdefghijklmnopqrstuvwxyz"
 	var sb strings.Builder
 	k := len(alphabet)
 
-	for i := 0; i < len; i++ {
+	for i := 0; i < strlen; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
@@ -26,13 +27,13 @@ func RandomStr(len int) string {
 }
 
 func RandomOwner() string {
-	return RandomStr(RandomInt(6, 12))
+	return RandomStr(int(RandomInt(6, 12)))
 }
 
-func RandomBalance() int {
+func RandomBalance() int64 {
 	return RandomInt(0, 50000)
 }
 
 func RandomCurrency() string {
-	return [3]string{"USD", "EUR", "CAD"}[RandomInt(0, 2)]
+	return [3]string{"USD", "EUR", "CAD"}[int(RandomInt(0, 2))]
 }
