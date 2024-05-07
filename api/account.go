927f788939caa844dddcfbcf10468502bd2372b1
Leon David Zipp <leondavidzipp@gmx.de>
2024-05-08 00:14:38 +0200

    added server.getAccount, fixed server.createAccount

--- api/account.go
+++ api/account.go
@@ -16,10 +16,7 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
 
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(
-			http.StatusBadRequest,
-			errorResponse(err),
-		)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return 
 	}
 
@@ -31,14 +28,34 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
-		ctx.JSON(
-			http.StatusInternalServerError,
-			errorResponse(err),
-		)
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
 	}
 
-	ctx.JSON(
-		http.StatusOK,
-		account,
-	)
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct{
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return 
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
 }
